bb52814c3c6e69f68f1e8dc34fed4a3e901095df
alderjunio <alder.junio@gmail.com>
2024-07-21 12:41:51 -0400

    edit index.js adicionando e removendo a classe selecionado

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+const botoes = document.querySelectorAll('.botao')
+
+botoes.forEach(botao =>{
+    botao.addEventListener("click", () => {
+        const botaoSelecionado = document.querySelector(".botao.selecionado")
+        botaoSelecionado.classList.remove("selecionado")
+
+        botao.classList.add("selecionado")
+    })
+})
+
+
